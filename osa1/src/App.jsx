fc1b5b20a22f685561041d69c01c598514912a93
Lauri <tikkalauri1@gmail.com>
2026-09-10 13:36:36 +0300

    osa1 a reactin alkeet

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,7 +1,37 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
 
+const App = () => {
+  const nimi = "Pekka"
+  const ika = 10
+
+  console.log('Hello from komponentti')
+
+  const now = new Date()
+  const a = 10
+  const b = 20
+
+  console.log(now, a + b)
+
+  return (
+    <div>
+      <p>Hello world</p>
+      <p>
+        {a} plus {b} is {a + b}
+      </p>
+      <Hello name="Lauri"/>
+      <Hello name="Jake"/>
+
+      <Hello name={nimi} age={ika}/>
+    </div>
+  )
+}
+
+/* Tärkee rivi älä poista */
 export default App
