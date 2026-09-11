9f55856215b09e8a1e5d14047d901bc3160876ff
Lauri <tikkalauri1@gmail.com>
2026-09-11 15:29:56 +0300

    Tehtävä 1.5

--- osa1/sovellus1.1/src/App.jsx
+++ osa1/sovellus1.1/src/App.jsx
@@ -34,27 +34,29 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course_name={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course_name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
